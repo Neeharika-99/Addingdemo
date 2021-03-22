dd78e7e89e77aa7c11ede4b0a8ac4e4e4f23f265
Neeharika <sai.neeharika99@gmail.com>
2021-03-22 16:42:33 +0530

    Add project files.

---
+++ Addingdemo/calci.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addingdemo
+{
+    class calci
+    {
+        int a=5;
+        int b=5;
+       
+    }
+}
